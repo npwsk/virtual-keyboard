f0876bbc4b9701e518ffadff5d23f3a38b57d64b
Nika Perfilova <ees2009.2014@gmail.com>
2022-05-06 19:53:02 +0700

    fix: change input field to textarea

--- js/views/inputField.js
+++ js/views/inputField.js
@@ -1,6 +1,5 @@
 const createInputField = () => {
-  const inputEl = document.createElement('input');
-  inputEl.type = 'text';
+  const inputEl = document.createElement('textarea');
   inputEl.classList.add('input-field');
   return inputEl;
 };
