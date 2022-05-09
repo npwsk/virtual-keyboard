b678305a0c578f9ae86878940f84b591fe23ab91
Nika Perfilova <ees2009.2014@gmail.com>
2022-05-09 22:29:36 +0700

    fix: input arrows when up/down arrow key is pressed

--- js/models/Keyboard.js
+++ js/models/Keyboard.js
@@ -55,8 +55,6 @@ class Keyboard {
       case keyCodes.BACKSPACE:
       case keyCodes.ARROW_LEFT:
       case keyCodes.ARROW_RIGHT:
-      case keyCodes.ARROW_UP:
-      case keyCodes.ARROW_DOWN:
       case keyCodes.CAPSLOCK:
         value = '';
         break;
